11700e28e30c8ae69f2894f2e269df0e60426b61
Evgenii Fedoseev <abap@sapville.com>
2022-02-08 11:37:32 -0800

    add DB constrans to posts

--- database/migrations/2022_01_29_122821_create_posts_table.php
+++ database/migrations/2022_01_29_122821_create_posts_table.php
@@ -15,8 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id');
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->restrictOnDelete()->restrictOnUpdate();
             $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
---
+++ database/migrations/2022_02_08_105827_posts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->restrictOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+        });
+    }
+}
