221b8eceb7d9f8fe86b0874e30853763a7948a38
Evgenii Fedoseev <abap@sapville.com>
2022-02-08 16:20:27 -0800

    posting comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Http\RedirectResponse;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['body', 'user_id'];
 
     public function post(): BelongsTo
     {
@@ -18,6 +21,20 @@ class Comment extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function store(Post $post): RedirectResponse
+    {
+        request()->validate([
+            'body' => ['required']
+        ]);
+
+        $post->comments()->create([
+            'body' => request('body'),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return back();
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\SessionController;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index']);
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/register', [RegistrationController::class, 'create'])->middleware('guest');
-Route::post('/register', [RegistrationController::class, 'store'])->middleware('guest');
+Route::post('posts/{post:slug}/comment', [Comment::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create'])->middleware('guest');
-Route::post('/login', [SessionController::class, 'store'])->middleware('guest');
+Route::get('register', [RegistrationController::class, 'create'])->middleware('guest');
+Route::post('register', [RegistrationController::class, 'store'])->middleware('guest');
 
-Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
